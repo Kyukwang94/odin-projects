300783a2ef40dd5274fa061cd8aa7c46a6cc366c
KyuKwangLee <dlrbrhkd94@naver.com>
2025-04-19 00:52:51 +0900

    DELETE ALL START AGAIN (trial 4)

--- calculator/script.js
+++ calculator/script.js
@@ -1,20 +1,5 @@
 import {add, subtract, multiply, divide, operate, operations} from './operator.js';
 
 //InitData
-const numBtns = Array.from(document.getElementsByClassName("digit-btn"));
-const operatorBtn = [document.getElementById("operate-btn")];
-const arithmeticBtn = Array.from(document.getElementsByClassName("arithmetic-btn"));
-const clearBtn = [document.getElementById("clear-btn")];
-const arrBtns = [...numBtns,...operatorBtn,...clearBtn,...arithmeticBtn];
 
-const displayEl = document.getElementById("displayer");
-const operators = ['+','-','*','/'];
-let storedData = ["0"];
-
-let valueLeft = [];
-let valueRight = [];
-
-let operator = "";
-
-//ButtonHandler
-
+//Button
