efb74aeb1d5815698396cb488a54454cc023f622
KyuKwangLee <dlrbrhkd94@naver.com>
2025-04-19 22:37:26 +0900

    JS COMPLETE!

--- calculator/script.js
+++ calculator/script.js
@@ -1,5 +1,120 @@
 import {add, subtract, multiply, divide, operate, operations} from './operator.js';
 
-//InitData
+let leftValue = null;
+let rightValue = null;
+let isrightValue = false;
+let result = 0;
+//Buttons
+let digitBtns = [...document.getElementsByClassName("digit-btn")];
+let arithmeticBtns = [...document.getElementsByClassName("arithmetic-btn")];
+let operateBtn = document.getElementById("operate-btn");
+let clearBtn = document.getElementById("clear-btn");
+let allBtns = [
+    ...digitBtns,
+    ...arithmeticBtns,
+    operateBtn,
+    clearBtn
+]
+const displayEl = document.getElementById("displayer");
+const operators = ["+", "-", "*", "/"];
+let operator = "";
+let reservedOperator = "";
+let storedData = ["0"];
 
-//Button
+allBtns.forEach((button)=>{
+    button.addEventListener('click',event=>{
+        let btnType = event.target.className || event.target.id;
+        if(btnType == 'digit-btn'){
+            console.log(button.textContent);
+            digitHandler(button);
+        }
+        if(btnType == 'arithmetic-btn'){
+            arithmeticHandler(button);
+        }
+        if(btnType == 'operate-btn'){
+            console.log("====OPERATE====")
+            arithemeticOperate();
+        }
+        if(btnType == 'clear-btn'){
+        }
+        display()
+        console.log(storedData);
+    })
+})
+function display(){
+    displayEl.textContent = storedData.join('');
+}
+function digitHandler(button){
+    const clickedDigit = button.textContent;
+
+    if(storedData[0] == "0" && !hasOperator()){
+        storedData[0] = clickedDigit;
+    }
+    else {
+        storedData.push(clickedDigit);
+    }
+}
+
+function arithmeticHandler(button){
+    const clickedOperator = button.textContent;
+    //NegativeNumber should't count as operator
+    if(!hasOperator()){
+        storedData.push(clickedOperator);
+    }
+    // 34 +(+,-,*,/)
+    else if(hasOperator() && findFirstOperator() == storedData.length -1){
+        storedData[storedData.length-1] = clickedOperator;
+    }
+    else{
+        console.log("===ARITHMETIC OPERATION===");
+        reservedOperator = clickedOperator;
+        arithemeticOperate();
+    }
+}
+function arithemeticOperate(){
+    //It's certain that there are both left and right values.
+    let leftArr = [];
+    let rightArr = [];
+    let resultArr = [];
+    let firstOperator = findFirstOperator();
+
+    if(!hasOperator()){
+        return;
+    }
+    //Left , Right
+    for(let i = 0; i < firstOperator; i++){
+        leftArr.push(storedData[i]);
+    }
+    for(let i = firstOperator + 1; i < storedData.length; i++){
+        rightArr.push(storedData[i]);
+    }
+    leftValue = Number(leftArr.join(''));
+    rightValue = Number(rightArr.join(''));
+    //====OPEARTE====
+    result = operate(operator,leftValue,rightValue);
+    console.log(`LV:${leftValue},RV:${rightValue}`);
+    console.log("RESULT: ",result);
+    //Set result to firstValue and Ready for next move.
+    firstOperator = reservedOperator;
+    leftValue = result;
+    storedData = [];
+    resultArr = result.toString().split('');
+    for(let i = 0; i < resultArr.length; i++){
+        storedData.push(resultArr[i]);
+    }
+    storedData.push(firstOperator);
+}
+function hasOperator(){
+    //Start from 1 because negative number doesn't count as operator
+    const hasOperator = storedData.slice(1).some(item => operators.includes(item));
+    return hasOperator;
+}
+function findFirstOperator(){
+    let index = storedData.findIndex((item,idx) => 
+        idx != 0 && operators.includes(item));
+    operator = storedData[index];
+
+    return index;
+}
+
+
